30dc78fa192d9b0471e57358b2316c8b625ef58e
Shivank Chaudhary <shivankchaudhary33@gmail.com>
2024-03-02 17:33:37 +0530

    Create squaresOfSortedArray.java

---
+++ squaresOfSortedArray.java
@@ -0,0 +1,14 @@
+// 977. Squares of a Sorted Array    (Leetcode)
+
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+
+        int n = nums.length;
+        int arr[] = new int[n];
+        for(int i=0 ; i < n ; i++){
+            arr[i] = nums[i] * nums[i];
+        }
+        Arrays.sort(arr);
+        return arr;
+    }
+}
