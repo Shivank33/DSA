3cd0f100766ebd9761656da5c3a890ff0c78f687
Shivank Chaudhary <shivankchaudhary33@gmail.com>
2024-03-11 09:45:01 +0530

    Create customSortString.java

---
+++ customSortString.java
@@ -0,0 +1,29 @@
+//   791. Custom Sort String   (Leetcode)
+
+
+class Solution {
+    public String customSortString(String order, String s) {
+    StringBuilder sb = new StringBuilder();
+    int[] count = new int[128];
+
+    for (final char c : s.toCharArray()){
+        count[c]++;
+    }
+
+    for (final char c : order.toCharArray()){
+        while (count[c] > 0){
+            sb.append(c);
+            count[c]--;
+        }
+    }
+
+    for (char c = 'a'; c <= 'z'; ++c){
+         while (count[c] > 0){
+             sb.append(c);
+             count[c]--;
+         }
+    }
+
+    return sb.toString();
+    }
+}
