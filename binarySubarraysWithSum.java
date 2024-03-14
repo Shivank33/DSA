82cda1ab984e45231c172895bc3845224d380322
Shivank Chaudhary <shivankchaudhary33@gmail.com>
2024-03-14 19:54:41 +0530

    Create binarySubarraysWithSum.java

---
+++ binarySubarraysWithSum.java
@@ -0,0 +1,18 @@
+//  930. Binary Subarrays With Sum  (Leetcode)
+
+
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        int[] cnt = new int[nums.length + 1];
+        cnt[0] = 1;
+        int ans = 0, s = 0;
+        for (int v : nums) {
+            s += v;
+            if (s - goal >= 0) {
+                ans += cnt[s - goal];
+            }
+            ++cnt[s];
+        }
+        return ans;
+    }
+}
