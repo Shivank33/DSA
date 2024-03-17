38bdbdba66f7487e1176054e7ac3275e2eeefefe
Shivank Chaudhary <shivankchaudhary33@gmail.com>
2024-03-17 08:47:05 +0530

    Create countPairsSumX.java

---
+++ countPairsSumX.java
@@ -0,0 +1,29 @@
+//   Count Pairs whose sum is equal to X   (GeeksforGeeks)
+
+
+class Solution {
+
+    public static int countPairs(LinkedList<Integer> head1, LinkedList<Integer> head2,
+                          int x) {
+        
+        HashSet<Integer> hs = new HashSet<>();
+        
+        int count = 0;
+        
+        Iterator<Integer> itr1 = head1.iterator();
+        while (itr1.hasNext())
+        {
+            hs.add(itr1.next());    
+        }
+        
+        Iterator<Integer> itr2 = head2.iterator();
+        while (itr2.hasNext())
+        {
+            if(hs.contains(x - itr2.next())){
+                count++;
+            } 
+        }
+    
+        return count;
+    }
+}
