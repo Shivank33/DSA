abde04f62fbd9c218a000484220d5343cfe84512
Shivank Chaudhary <shivankchaudhary33@gmail.com>
2024-03-20 21:05:18 +0530

    Create sumNodesFromRootToLeaf.java

---
+++ sumNodesFromRootToLeaf.java
@@ -0,0 +1,43 @@
+//     Sum of nodes on the longest path from root to leaf node    (GeeksforGeeks)
+
+
+/*
+node class of binary tree
+class Node {
+    int data;
+    Node left, right;
+    
+    public Node(int data){
+        this.data = data;
+    }
+}
+*/
+class Solution{
+    public int sumOfLongRootToLeafPath(Node root)
+    {
+        if(root==null){
+            return 0;
+        }
+        int depth = findDepth(root);
+        return findLongestPathSum(root, depth);
+    }
+    
+    public int findDepth(Node node){
+        if(node==null){
+            return 0;
+        } 
+        return 1+ Math.max(findDepth(node.left),findDepth(node.right));
+    }
+    
+    public int findLongestPathSum(Node node, int depth){
+        
+        if(node==null){
+            return Integer.MIN_VALUE;
+        } else if(depth==1){
+            return node.data;
+        } else {
+            return node.data+
+            Math.max(findLongestPathSum(node.left,depth-1),findLongestPathSum(node.right,depth-1));
+        }
+    }
+}
