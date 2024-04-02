be290f083cee7edeb3a5a6c2a6f4d0ccee3b3f16
Shivank Chaudhary <shivankchaudhary33@gmail.com>
2024-04-02 21:06:21 +0530

    Create minimumAbsoluteDifferenceInBST.java

---
+++ GeeksforGeeks/minimumAbsoluteDifferenceInBST.java
@@ -0,0 +1,29 @@
+// Minimum Absolute Difference In BST    (GeeksforGeeks)
+
+
+
+/*The Node structure is defined as
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+};
+*/
+
+int prev = Integer.MAX_VALUE, ans = Integer.MAX_VALUE;
+    int absolute_diff(Node root)
+    {
+        inorder(root);
+        return ans;
+    }
+    public void inorder(Node root){
+        if(root==null) return;
+        inorder(root.left);
+        
+        if(prev!=Integer.MAX_VALUE){
+            ans=Math.min(ans,root.data-prev);
+        }
+        prev=root.data;
+        inorder(root.right);
+    }
